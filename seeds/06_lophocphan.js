5a3d41db9ebe303bfa00926d929918c661733bc8
ruamin <huyentrang13101997@gmail.com>
2019-11-17 10:58:45 +0700

    update seeds

--- seeds/06_lophocphan.js
+++ seeds/06_lophocphan.js
@@ -6,6 +6,12 @@ exports.seed = function(knex, Promise) {
       // Inserts seed entries
       return knex('lophocphan').insert([
         {id: 1, malophocphan:'12264151 1 ',idmonhoc:'1', idkyhoc:'1'},
+        {id: 17, malophocphan:'12226151 1 ',idmonhoc:'6', idkyhoc:'1'},
+        {id: 18, malophocphan:'12264151 1 ',idmonhoc:'6', idkyhoc:'1'},
+        {id: 19, malophocphan:'12264151 1 ',idmonhoc:'6', idkyhoc:'1'},
+        {id: 21, malophocphan:'12264151 1 ',idmonhoc:'2', idkyhoc:'1'},
+        {id: 29, malophocphan:'12264151 1 ',idmonhoc:'1', idkyhoc:'1'},
+        {id: 30, malophocphan:'12264151 1 ',idmonhoc:'4', idkyhoc:'1'},
       ]);
     });
 };
