e20791b10fd8ac4163fd5755e5f19c02c5940449
ruamin <huyentrang13101997@gmail.com>
2019-12-01 10:27:00 +0700

    feat: update somethings

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-const app = require('./src/app')
+const app = require("./src/app");
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
-app.listen(port, () => { console.log(`App is running on port ${port}`) })
+app.listen(port, () => {
+  console.log(`App is running on port ${port}`);
+});
